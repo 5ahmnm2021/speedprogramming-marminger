5cbc8cb19cc533045cc2601cff7e48bf7669d793
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2021-04-13 09:43:46 +0200

    Finale Abgabe mit Update Readme

---
+++ Assets/Script/WrongAnswers.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WrongAnswers : MonoBehaviour
+{
+    public int answers;
+
+    public void QuestionOneRight()
+    {
+        StartCoroutine(LoadQuestionOneRight());
+    }
+
+    public void QuestionTwoRight()
+    {
+        StartCoroutine(LoadQuestionTwoRight());
+    }
+
+    public void AnswerRight()
+    {
+        StartCoroutine(LoadAnswerRight());
+    }
+
+
+    IEnumerator LoadQuestionOneRight()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Szene2");
+    }
+
+    IEnumerator LoadQuestionTwoRight()
+    {
+        answers = answers + 1;
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Szene3");
+    }
+
+    IEnumerator LoadAnswerRight()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Szene1");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # speedprogramming-marminger
+
+Die Anzahl, wieviele Antworten richtig sind geht nicht.
+Die Zeit war zu knapp- da ich zu Beginn Probleme mit dem Internet und Git hatte
