1b6c03036a1347c6a72c10c85a31f6dbd6f24c13
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2021-04-13 09:23:08 +0200

    Scene Switches erstellt

---
+++ Assets/Script/SceneSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwitch : MonoBehaviour
+{
+
+        public void LoadSzene2()
+        {
+            SceneManager.LoadScene("Szene2");
+        }
+
+        public void LoadSzene3()
+        {
+            SceneManager.LoadScene("Szene3");
+        }
+
+        public void LoadSzene1()
+        {
+            SceneManager.LoadScene("Szene1");
+        }
+    
+}
